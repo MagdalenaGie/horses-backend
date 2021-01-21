fe34d54a92fc5fc94d63b7e8adbae1e3e0fb44fe
MagdalenaGie <magdalenaxgorska@gmail.com>
2021-01-21 18:45:17 +0100

    port changed

--- index.js
+++ index.js
@@ -443,6 +443,6 @@ app
   .delete('/kontuzje/:idkon/:idkontuzja', deleteInjury)
   .delete('/konie', deleteHorse)
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("server has started on port 5000");
   });
